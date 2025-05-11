a79358455d12b399df3ea73b6377c47366c71545
Jariullah <112340610+Mohdjariullah@users.noreply.github.com>
2025-05-11 21:28:18 +0530

    Added Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Analytics } from "@vercel/analytics/next"
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import Hero from "@/components/home/hero";
@@ -8,6 +8,7 @@ import WhyChooseAIdaptics from "@/components/home/whychoose";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-black text-white">
+      <Analytics />
       <Navbar />
 
       <Hero />
