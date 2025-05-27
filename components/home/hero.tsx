48b067bd081ce44a51fa34aa773247c65bbbdaa2
Mohdjariullah <112340610+Mohdjariullah@users.noreply.github.com>
2025-05-27 22:01:02 +0530

    Update hero.tsx

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { Application } from "@splinetool/runtime";
 import Image from "next/image";
 import HeroGif from "@/public/quantum (1).gif";
+import Link from "next/link";
 
 const Hero: React.FC = () => {
   const [canHandle3D, setCanHandle3D] = useState<boolean>(true);
@@ -300,17 +301,19 @@ const Hero: React.FC = () => {
       >
         We turn Complex ideas into effortless solutions.
       </motion.p>
-      <motion.button
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, delay: 1 }}
-        className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-      >
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-          Get Started
-        </span>
-      </motion.button>
+      <Link href="/get-started" passHref legacyBehavior>
+        <motion.a
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Get Started
+          </span>
+        </motion.a>
+      </Link>
       </motion.div>
     </div>
   );
